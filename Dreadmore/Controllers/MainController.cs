89fd1e10b2cad8ac49c234dfcc35c690cefe1e8c
Craig Coppola <craig.coppola@archetypesc.com>
2019-03-13 10:54:02 -0400

    Setup

---
+++ Dreadmore/Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Web.Http;
+using System.Web.Mvc;
+using Dreadmore.DTOModels;
+using Dreadmore.Managers;
+
+namespace Dreadmore.Controllers
+{
+    public class MainController : Controller
+    {
+
+        SiteManager site = new SiteManager();
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/Main/GetQuote/")]
+        public ActionResult GetQuote()
+        {
+            var quote = site.GetQuote();
+            return View(quote);
+        }
+    }
+}
---
+++ Dreadmore/Managers/SiteManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dreadmore;
+using Dreadmore.DTOModels;
+
+namespace Dreodmare.Managers
+{
+    public class SiteManager
+    {
+        DMDB db = new DMDB();
+        public Quotes GetQuote()
+        {
+            List<quote_Quote> list = db.quote_Quote.ToList();
+            int n = list.Count;
+            Random rnd = new Random();
+            int i = rnd.Next(0, n);
+            quote_Quote dbQuote = list[i];
+
+            Quotes quote = new Quotes()
+            {
+                Quote = dbQuote.Quote,
+                QuoteBy = dbQuote.QuoteBy,
+                QuoteByImage = dbQuote.QuoteByImage
+            };
+
+            return quote;
+        }
+
+    }
+}
