fb3b0242f72e87251428ad79e248dc5fda0c0f10
Craig Coppola <craig.coppola@archetypesc.com>
2019-03-13 10:28:19 -0400

    Add project files.

---
+++ Dreadmore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dreadmore.Startup))]
+namespace Dreadmore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
